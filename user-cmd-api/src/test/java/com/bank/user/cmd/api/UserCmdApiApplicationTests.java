fc9184a4588a4a7deb9e527d5e0f89c30d87e35a
mohamed benayad <mohamed.benayad@adria-bt.com>
2021-10-05 12:04:15 +0100

    project init

---
+++ user-cmd-api/src/test/java/com/bank/user/cmd/api/UserCmdApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.user.cmd.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCmdApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ user-query-api/src/test/java/com/bank/userqueryapi/UserQueryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.userqueryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserQueryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
